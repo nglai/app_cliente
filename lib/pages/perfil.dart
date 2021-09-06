6e8d42e3f5d66179d7a64baecd47c51a1b345f28
marcussenise <marcus.senise@hotmail.com>
2021-09-06 16:16:00 -0300

    Bottom navigator

---
+++ lib/pages/perfil.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Perfil extends StatefulWidget {
+  Perfil({Key? key}) : super(key: key);
+
+  @override
+  _PerfilState createState() => _PerfilState();
+}
+
+class _PerfilState extends State<Perfil> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Text('carrinho'),
+    );
+  }
+}
