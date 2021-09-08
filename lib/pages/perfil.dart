cb12d7d9c6616a00f897999fd6bc076668a01009
marcussenise <marcus.senise@hotmail.com>
2021-09-08 10:35:05 -0300

    Update perfil.dart

--- lib/pages/perfil.dart
+++ lib/pages/perfil.dart
@@ -13,63 +13,66 @@ class Perfil extends StatefulWidget {
 class _PerfilState extends State<Perfil> {
   @override
   Widget build(BuildContext context) {
-
-  final textStyles = Theme.of(context).textTheme;
+    final textStyles = Theme.of(context).textTheme;
     return Scaffold(
-       appBar: AppBar(
-         title: Text('Perfil'),
-        ),
-        body: Column(
-          children: [
+      appBar: AppBar(
+        title: Text('Perfil'),
+      ),
+      body: Column(
+        children: [
           Container(
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25)),
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25)),
               color: Color.fromRGBO(224, 170, 255, 1),
-            ),  
+            ),
             height: 200,
             alignment: Alignment.center,
             child: Column(
-              children:[
-                SizedBox(height: 25,),
-                CircleAvatar(child: FaIcon(FontAwesomeIcons.userNinja),radius: 50,),  
-                SizedBox(height: 20,),
-                Text("Olá, fulano!", style: textStyles.headline5,),
+              children: [
+                SizedBox(
+                  height: 25,
+                ),
+                CircleAvatar(
+                  child: FaIcon(FontAwesomeIcons.userNinja),
+                  radius: 50,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Olá, fulano!",
+                  style: textStyles.headline5,
+                ),
                 //quando userController tiver pronto:
                 //CircleAvatar(userController.model.foto) ??? (confirmar)
                 //Text("Olá, ${userController.model.nome}!")
-              ], 
+              ],
             ),
           ),
-              SizedBox(height: 20),
-              ListTile(
-                leading: FaIcon(FontAwesomeIcons.userCircle),
-                title: Text('Minha conta'),
-                trailing: FaIcon(FontAwesomeIcons.angleRight),
-                onTap: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MinhaConta())
-                    );
-                },
-              ),
-              SizedBox(height: 20),
-               ListTile(
-                leading: FaIcon(FontAwesomeIcons.clock),
-                title: Text('Histórico'),
-                trailing: FaIcon(FontAwesomeIcons.angleRight),
-                onTap: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Historico())
-                    );
-                },
-              )
-
- 
+          SizedBox(height: 20),
+          ListTile(
+            leading: FaIcon(FontAwesomeIcons.userCircle),
+            title: Text('Minha conta'),
+            trailing: FaIcon(FontAwesomeIcons.angleRight),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => MinhaConta()));
+            },
+          ),
+          SizedBox(height: 20),
+          ListTile(
+            leading: FaIcon(FontAwesomeIcons.clock),
+            title: Text('Histórico'),
+            trailing: FaIcon(FontAwesomeIcons.angleRight),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Historico()));
+            },
+          )
         ],
-        )
-         
-    
-    
+      ),
+    );
   }
 }
