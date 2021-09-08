5ba0e726dc8b05afe6404eebfb51775e381bd177
bruno-bergamaschi <brunobergamaschi@live.com>
2021-09-08 11:29:50 -0300

    Ajuste na tela de login

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,33 @@
+import 'package:app_cliente/pages/login_page.dart';
+import 'package:app_cliente/pages/menu_page.dart';
+
+import '../controllers/user_controller.dart';
+import 'login_page.dart';
+import '../widgets/splash_loading_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SplashPage extends StatefulWidget {
+  SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<UserController>(
+      builder: (context, userController, child) {
+        switch (userController.authState) {
+          case AuthState.signed:
+            return MenuPage();
+          case AuthState.unsigned:
+            return LoginPage();
+          case AuthState.loading:
+            return SplashLoadingWidget();
+        }
+      },
+    );
+  }
+}
